6e268cc8844e99084b8fe72cf86268c0ef28a222
Abdul Kalam Syed <abdulksyed10@gmail.com>
2023-05-08 17:49:53 -0700

    added a comment to test

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from PyQt5 import QtWidgets
 import imageUiAttached
 
+#needed to run the program
 app = QtWidgets.QApplication(sys.argv)
 imageUI_win = QtWidgets.QMainWindow()
 imageUI_ui = imageUiAttached.imageUiAttached(imageUI_win)
