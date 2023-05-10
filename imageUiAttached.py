951b11ac568db76d81fae81d3ef5cd6b890a485f
Abdul Kalam Syed <abdulksyed10@gmail.com>
2023-05-09 20:12:00 -0700

    Added the learning model

--- imageUiAttached.py
+++ imageUiAttached.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+'''from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
 from imageUi import Ui_MainWindow
 
 class imageUiAttached(Ui_MainWindow, QMainWindow):
@@ -11,3 +11,4 @@ class imageUiAttached(Ui_MainWindow, QMainWindow):
     
     def submitBtnClicked(self):
         self.outputTxt.setText("Hello, you still got a lot more things to do!!!")
+'''
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys
+'''import sys
 from PyQt5 import QtWidgets
 import imageUiAttached
 
@@ -7,3 +7,66 @@ app = QtWidgets.QApplication(sys.argv)
 imageUI_win = QtWidgets.QMainWindow()
 imageUI_ui = imageUiAttached.imageUiAttached(imageUI_win)
 app.exec_()
+'''
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.python import keras
+from keras import datasets, layers, models
+import random
+
+(training_images, training_labels), (testing_images, testing_labels) = datasets.cifar10.load_data()
+training_images, testing_images = training_images / 255, testing_images / 255
+
+class_names = ['Plane', 'Car', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck']
+
+'''
+for i in range(16):
+    plt.subplot(4,4,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(training_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[training_labels[i][0]])
+
+plt.show()
+'''
+
+training_images = training_images[:200000]
+training_labels = training_labels[:200000]
+testing_images = testing_images[:4000]
+testing_labels = testing_labels[:4000]
+
+'''
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)))
+model.add(layers.MaxPooling2D(2,2))
+model.add(layers.Conv2D(64, (3,3), activation='relu'))
+model.add(layers.MaxPooling2D(2,2))
+model.add(layers.Conv2D(64, (3,3), activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(training_images, training_labels, epochs=10, validation_data=(testing_images, testing_labels))
+
+loss, accuracy = model.evaluate(testing_images, testing_labels)
+print(f"Loss: {loss}")
+print(f"Accuracy: {accuracy}")
+
+model.save('image_classifier.model')
+'''
+
+model = models.load_model('image_classifier.model')
+
+#img = random.randint(0, len(testing_images))
+img = cv.imread('watch.jpg')
+img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+
+plt.imshow(img, cmap=plt.cm.binary)
+
+prediction = model.predict(np.array([img]) / 255)
+index = np.argmax(prediction)
+print(f'Prediction is {class_names[index]}')
+
+plt.show()
